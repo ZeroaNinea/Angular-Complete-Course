61fd1597fe1122d648ed6a513bbd53547a778f96
ZeroaNinea <zeroaninea@gmail.com>
2024-08-29 14:21:08 +0400

    thrid lesson

--- financial-logger/src/app/app.component.ts
+++ financial-logger/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+import { provideStore } from "@ngrx/store";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  // providers: [provideStore()],
 })
 export class AppComponent {
-  title = 'financial-logger';
+  title = "financial-logger";
 }
