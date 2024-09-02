a9a78e732f1e86df7d1f12421b6e99c842319f43
ZeroaNinea <zeroaninea@gmail.com>
2024-09-02 23:22:38 +0400

    home component imported

---
+++ ecomm/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  template: `
+    Hello from AppComponent
+    <app-home></app-home>
+  `,
+  styleUrl: "./app.component.scss",
+})
+export class AppComponent {
+  title = "ecomm";
+}
