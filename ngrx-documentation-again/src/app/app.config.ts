ad222e3d5bfae25ac753a2a794da14bf6a18774b
ZeroaNinea <zeroaninea@gmail.com>
2024-10-12 03:12:33 +0400

    redux devtools addded

--- ngrx-documentation-again/src/app/app.config.ts
+++ ngrx-documentation-again/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
@@ -12,6 +16,7 @@ import { CounterEffects } from "./state/counter.effect";
 
 import { scoreboardReducer } from "./state/scoreboard/scoreboard.reducer";
 import { scoreboardFeatureKey } from "./state/scoreboard/scoreboard.state";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,6 +32,13 @@ export const appConfig: ApplicationConfig = {
     // Scoreboard
     // provideStore({ game: scoreboardReducer }),
     provideState({ name: scoreboardFeatureKey, reducer: scoreboardReducer }),
-    // /Scoreboard
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true, // If set to true, the connection is established within the Angular zone
+    }),
   ],
 };
