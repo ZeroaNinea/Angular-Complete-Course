e80b14497f58b20ff82202dd22b2a9ae29896444
ZeroaNinea <zeroaninea@gmail.com>
2024-10-20 22:30:28 +0400

    typography lesson complete

---
+++ angular-material-freecodecamp/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+const MaterialComponents = [MatButtonModule, MatSlideToggleModule];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
