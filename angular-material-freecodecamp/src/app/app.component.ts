082391d89d56efaae362bc85f1ae072b19cd2d94
ZeroaNinea <zeroaninea@gmail.com>
2024-10-21 03:53:04 +0400

    sixth lesson finished

--- angular-material-freecodecamp/src/app/app.component.ts
+++ angular-material-freecodecamp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material/material.module';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaterialModule],
+  imports: [RouterOutlet, MaterialModule, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- angular-material-freecodecamp/src/app/material/material.module.ts
+++ angular-material-freecodecamp/src/app/material/material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-const MaterialComponents = [MatButtonModule, MatSlideToggleModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatSlideToggleModule,
+  MatButtonToggleModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
