9aea20d9bf0a4155468ad109a35ff3cb50941a05
ZeroaNinea <zeroaninea@gmail.com>
2024-10-20 20:08:35 +0400

    the angular-material-freecodecamp recreated

--- angular-material-freecodecamp/src/app/app.component.spec.ts
+++ angular-material-freecodecamp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-material-documentation' title`, () => {
+  it(`should have the 'angular-material-freecodecamp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-material-documentation');
+    expect(app.title).toEqual('angular-material-freecodecamp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-material-documentation');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-material-freecodecamp');
   });
 });
