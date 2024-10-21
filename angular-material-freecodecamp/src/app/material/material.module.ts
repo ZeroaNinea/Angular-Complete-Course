154324e475a564010fdf4cd5a2a596f7c8e169ae
ZeroaNinea <zeroaninea@gmail.com>
2024-10-21 05:03:21 +0400

    seventh lesson completed

--- angular-material-freecodecamp/src/app/material/material.module.ts
+++ angular-material-freecodecamp/src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatButtonModule,
   MatSlideToggleModule,
   MatButtonToggleModule,
+  MatIconModule,
 ];
 
 @NgModule({
