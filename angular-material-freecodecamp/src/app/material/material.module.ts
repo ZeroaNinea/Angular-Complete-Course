fe239ac40e461fae7f088bb2cd1ffb41893bb68c
ZeroaNinea <zeroaninea@gmail.com>
2024-10-28 23:59:24 +0400

    twenty second lesson completed

--- angular-material-freecodecamp/src/app/material/material.module.ts
+++ angular-material-freecodecamp/src/app/material/material.module.ts
@@ -24,6 +24,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { map, Observable, startWith } from 'rxjs';
 
@@ -50,6 +52,8 @@ const MaterialComponents = [
   MatInputModule,
   MatSelectModule,
   MatAutocompleteModule,
+  MatCheckboxModule,
+  MatRadioModule,
 ];
 
 @NgModule({
