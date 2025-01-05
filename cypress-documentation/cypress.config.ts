9e0e30165aa900faa3c2f7f5ed6e24270287a45a
Catgirl <zeroaninea@gmail.com>
2025-01-05 22:17:20 +0400

    baseurl added in config file

--- cypress-documentation/cypress.config.ts
+++ cypress-documentation/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:4200", // The URL where Cypress should test. It allows to specify relative URL in `cy.ts` files. E.g., `/` instead `http://localhost:4200/`.
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
