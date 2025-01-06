67d4607f882f543e3e857506d9233d0d6ac215ac
Catgirl <zeroaninea@gmail.com>
2025-01-06 15:15:26 +0400

    cypress-documentation-core-concepts created

---
+++ cypress-documentation-core-concepts/README.md
@@ -0,0 +1,21 @@
+# CypressDocumentationCoreConcepts
+
+This is a project to learn Cypress using Angular with the official [documentatation](https://docs.cypress.io/app/core-concepts/introduction-to-cypress). I had problems with dependencies in Angular 19, so I created a new project.
+
+To run this projcet type:
+
+```bash
+npm start
+```
+
+Or:
+
+```bash
+ng s -o
+```
+
+To run Cypress type:
+
+```bash
+npm run cypress:open
+```
---
+++ cypress-documentation-core-concepts/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'cypress-documentation-core-concepts';
+}
