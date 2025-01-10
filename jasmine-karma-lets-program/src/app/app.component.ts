40b20dede4795995a59d334c009133b7d101dc97
Catgirl <zeroaninea@gmail.com>
2025-01-10 19:17:15 +0400

    import of the app-table removed form the app component

--- jasmine-karma-lets-program/src/app/app.component.ts
+++ jasmine-karma-lets-program/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TableComponent],
+  imports: [RouterOutlet, HeaderComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
