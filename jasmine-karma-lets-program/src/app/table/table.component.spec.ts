d51df73a8ca135acb8ff7c4a906844e9e4667e42
Catgirl <zeroaninea@gmail.com>
2025-01-10 19:14:23 +0400

    i gonna learn documentation

--- jasmine-karma-lets-program/src/app/table/table.component.spec.ts
+++ jasmine-karma-lets-program/src/app/table/table.component.spec.ts
@@ -21,4 +21,16 @@ describe('TableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set buttontext to Copied and selectedRow to to be copied no successfull copy', async () => {
+    const sampleTextToCopy = 'Sample Text';
+    spyOn(navigator.clipboard, 'writeText').and.returnValue(Promise.resolve());
+
+    component.copyToClipboard(sampleTextToCopy);
+
+    await navigator.clipboard.writeText(sampleTextToCopy);
+
+    expect(component.selectedRow()).toBe(sampleTextToCopy);
+    expect(component.buttonText()).toBe('Copied');
+  });
 });
