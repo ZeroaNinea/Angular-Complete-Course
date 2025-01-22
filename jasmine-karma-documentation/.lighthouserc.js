767464dcff90ea32cd0b6037dd1d81b9645891ea
Catgirl <zeroaninea@gmail.com>
2025-01-23 02:11:10 +0400

    trying to run the angular project

--- jasmine-karma-documentation/.lighthouserc.js
+++ jasmine-karma-documentation/.lighthouserc.js
@@ -1,23 +1,26 @@
-// module.exports = {
-//   ci: {
-//     collect: {
-//       url: ["http://localhost:8080"],
-//       startServerCommand:
-//         "http-server ./dist/jasmine-karma-documentation -p 8080",
-//       startServerReadyPattern: "Available on",
-//       startServerReadyTimeout: 10000,
-//       numberOfRuns: 1,
-//     },
-//     assert: {
-//       assertions: {
-//         "categories:performance": ["warn", { minScore: 0.6 }],
-//         "categories:accessibility": ["warn", { minScore: 0.7 }],
-//         "categories:best-practices": ["warn", { minScore: 0.9 }],
-//         "categories:seo": ["warn", { minScore: 0.9 }],
-//       },
-//     },
-//     upload: {
-//       target: "temporary-public-storage",
-//     },
-//   },
-// };
+module.exports = {
+  ci: {
+    // collect: {
+    //   url: ["http://localhost:8080"],
+    //   startServerCommand:
+    //     "http-server ./dist/jasmine-karma-documentation -p 8080",
+    //   startServerReadyPattern: "Available on",
+    //   startServerReadyTimeout: 10000,
+    //   numberOfRuns: 1,
+    // },
+    collect: {
+      staticDistDir: "./src",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.6 }],
+        "categories:accessibility": ["warn", { minScore: 0.7 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
