5785a4895aba77911ae9bf00771250aa940d1203
Catgirl <zeroaninea@gmail.com>
2025-01-22 23:23:08 +0400

    fixing configurations in .lighthouse.js

--- jasmine-karma-documentation/.lighthouserc.js
+++ jasmine-karma-documentation/.lighthouserc.js
@@ -1,26 +1,3 @@
-// {
-//   "ci": {
-//     "collect": {
-//       "startServerCommand": "npm start",
-//       "startServerReadyPattern": "listening|ready|open",
-//       "url": ["http://localhost:4200"],
-//       "numberOfRuns": 1
-//     },
-//     "assert": {
-//       "bf-cache": ["warn", { "minScore": 0.5 }],
-//       "meta-description": ["warn", { "minScore": 0.5 }],
-//       "total-byte-weight": ["error", { "maxNumericValue": 300000 }],
-//       "unminified-javascript": "warn",
-//       "unused-css-rules": "warn",
-//       "uses-text-compression": "warn",
-//       "preset": "lighthouse:recommended",
-//       "startServerReadyPattern": "compiled successfully|waiting for changes"
-//     },
-//     "upload": {
-//       "target": "temporary-public-storage"
-//     }
-//   }
-// }
 module.exports = {
   ci: {
     collect: {
@@ -33,10 +10,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        // "categories:performance": ["error", { minScore: 0.9 }],
-        // "categories:accessibility": ["warn", { minScore: 0.9 }],
-        "categories:performance": ["warn", { minScore: 0.75 }],
-        "categories:accessibility": ["warn", { minScore: 0.8 }],
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        // "categories:performance": ["warn", { minScore: 0.75 }],
+        // "categories:accessibility": ["warn", { minScore: 0.8 }],
         "categories:best-practices": ["warn", { minScore: 0.9 }],
         "categories:seo": ["warn", { minScore: 0.9 }],
       },
