889f9b671c15686b6f0bbfad67d9b6cd10e7a53a
Catgirl <zeroaninea@gmail.com>
2025-01-22 23:27:47 +0400

    fixing configurations in .lighthouse.js

--- jasmine-karma-documentation/.lighthouserc.js
+++ jasmine-karma-documentation/.lighthouserc.js
@@ -11,9 +11,9 @@ module.exports = {
     assert: {
       assertions: {
         // "categories:performance": ["error", { minScore: 0.9 }],
-        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        // "categories:accessibility": ["warn", { minScore: 0.9 }],
         "categories:performance": ["warn", { minScore: 0.75 }],
-        // "categories:accessibility": ["warn", { minScore: 0.8 }],
+        "categories:accessibility": ["warn", { minScore: 0.8 }],
         "categories:best-practices": ["warn", { minScore: 0.9 }],
         "categories:seo": ["warn", { minScore: 0.9 }],
       },
