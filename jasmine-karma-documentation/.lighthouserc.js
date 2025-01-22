821b21fc9e6970e617aa497a5d20d83872531ba6
Catgirl <zeroaninea@gmail.com>
2025-01-23 00:01:56 +0400

    GitHub token added

--- jasmine-karma-documentation/.lighthouserc.js
+++ jasmine-karma-documentation/.lighthouserc.js
@@ -10,10 +10,6 @@ module.exports = {
     },
     assert: {
       assertions: {
-        // "categories:performance": ["error", { minScore: 0.9 }],
-        // "categories:accessibility": ["warn", { minScore: 0.9 }],
-        // "categories:performance": ["warn", { minScore: 0.75 }],
-        // "categories:accessibility": ["warn", { minScore: 0.8 }],
         "categories:performance": ["warn", { minScore: 0.6 }],
         "categories:accessibility": ["warn", { minScore: 0.7 }],
         "categories:best-practices": ["warn", { minScore: 0.9 }],
